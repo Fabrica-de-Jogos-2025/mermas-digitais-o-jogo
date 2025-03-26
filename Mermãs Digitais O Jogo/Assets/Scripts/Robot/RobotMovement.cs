85e52d2f9691e6f28df96c5d3615ad42521e2090
JRMadara <joser.almir.324@gmail.com>
2025-03-26 01:40:27 -0300

    Feat: Fixed bot attack.

--- Mermãs Digitais O Jogo/Assets/Scripts/Robot/RobotMovement.cs
+++ Mermãs Digitais O Jogo/Assets/Scripts/Robot/RobotMovement.cs
@@ -18,7 +18,9 @@ public class RobotMovement : MonoBehaviour
       get { return respawnpoint; } 
       set { respawnpoint = value; } 
     }
-
+    public float direction;
+    public Vector3 moviment;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -34,7 +36,7 @@ public class RobotMovement : MonoBehaviour
     {
         if (!isUsingPowerUp && Vector2.Distance(transform.position, Target.position) >= StoppingDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, Target.position + moviment, Speed * Time.deltaTime);
         }
 
         float rotation = Input.GetAxis("Horizontal");
@@ -42,12 +44,17 @@ public class RobotMovement : MonoBehaviour
         if (rotation > 0)
         {
             transform.eulerAngles = new Vector2(0f, 0f);
+            moviment = new Vector3(-1f, 0.5f, 0f);
         } else if (rotation < 0)
         {
             transform.eulerAngles = new Vector2(0f, 180f);
+            moviment = new Vector3(1f, 0.5f, 0f);
         }
 
+        if(Vector2.Distance(transform.position, Target.position) <= 2)
+        {
         PowerUp();
+        }
     }
 
     public void Die()
@@ -69,39 +76,74 @@ public class RobotMovement : MonoBehaviour
     IEnumerator UsePowerUp()
     {
         isUsingPowerUp = true;
-        float direction = player.transform.localScale.x;
+        //float direction = player.transform.localScale.x;
+        direction = player.transform.position.x - transform.position.x;
         float maxDistance;
-        Vector3 originalPosition = transform.position;
+        //Vector3 originalPosition = transform.position;
 
 
         if (direction > 0)
         {
-            maxDistance = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, 0)).x;
-        } else
-        {
-            maxDistance = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, 0)).x;
-        }
+            maxDistance = Camera.main.ViewportToWorldPoint(new Vector3(0.75f, 0.5f, 0)).x;
+            
+            while (direction > 0 && transform.position.x < maxDistance)
+            {
+                transform.position += Vector3.right * Mathf.Sign(direction) * Speed * Time.deltaTime; // Move o robo para a direita
 
-        while ((direction > 0 && transform.position.x < maxDistance) || (direction < 0 && transform.position.x > maxDistance))
-        {
-                transform.position += Vector3.right * Mathf.Sign(direction) * Speed * Time.deltaTime; // Move o robô para a direita
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, 1f);
+                foreach (Collider2D enemy in enemies)
+                {
+                    if (enemy.CompareTag("Enemy"))
+                    {
+                        Destroy(enemy.gameObject);
+                    }
+                }
+
                 yield return null;
+            }
+
+        } else if (direction <= 0)
+        {
+            maxDistance = Camera.main.ViewportToWorldPoint(new Vector3(0.25f, 0.5f, 0)).x;
+
+            while (direction <= 0 && transform.position.x > maxDistance)
+            {
+                transform.position += Vector3.left * Mathf.Sign(-direction) * Speed * Time.deltaTime; // Move o robo para a esquerda
+
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, 1f);
+                foreach (Collider2D enemy in enemies)
+                {
+                    if (enemy.CompareTag("Enemy"))
+                    {
+                        Destroy(enemy.gameObject);
+                    }
+                }
+
+                yield return null;
+            }
+
         }
 
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, 5f);
+        /*while ((direction > 0 && transform.position.x < maxDistance) || (direction < 0 && transform.position.x > maxDistance))
+        {
+                transform.position += Vector3.right * Mathf.Sign(direction) * Speed * Time.deltaTime; // Move o robï¿½ para a direita
+                yield return null;
+        }*/
+
+        /*Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, 5f);
         foreach (Collider2D enemy in enemies)
         {
             if (enemy.CompareTag("Enemy"))
             {
                 Destroy(enemy.gameObject);
             }
-        }
+        }*/
 
-        while (Vector2.Distance(transform.position, originalPosition) > 0.1f)
+        /*while (Vector2.Distance(transform.position, originalPosition) > 0.1f)
         {
             transform.position = Vector2.MoveTowards(transform.position, originalPosition, Speed * Time.deltaTime);
             yield return null;
-        }
+        }*/
 
         isUsingPowerUp = false;
     }
