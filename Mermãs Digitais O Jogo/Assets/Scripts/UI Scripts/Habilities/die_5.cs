e3f7d657b9c33bc5b817c5781f2f3a79354ed041
keniareis <keniapubg@gmail.com>
2025-03-27 20:59:05 -0300

    add: margaret hability and dialogs

---
+++ Mermãs Digitais O Jogo/Assets/Scripts/UI Scripts/Habilities/die_5.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Die5 : MonoBehaviour
+{
+    public Hability5 Dest;
+    public float destroyInSec = 3f;
+    private bool isDestroyed = false;
+    public GameObject rocket; // Referência ao foguete
+    [SerializeField] private GameObject rocketCollider; // Collider do foguete
+
+    public Hability5 Dest2;
+    
+    void Start()
+    {
+        // Obtém o Collider do foguete no início do jogo
+    }
+
+
+    void Update()
+    {
+
+        if (Dest.IsCorrectlyPlaced())
+        {
+            StartCoroutine(DestroyAfterDelay());
+        } 
+
+        // Verifica se todas as peças estão corretamente posicionadas
+        if (!isDestroyed && Hability5.allDraggableObjects.TrueForAll(obj => obj.IsCorrectlyPlaced()) && Dest2.puzzleSolved)
+        {
+            StartCoroutine(DestroyAfterDelay());
+            isDestroyed = true; // Evita chamadas repetidas da coroutine
+            isDestroyed = true;
+            Destroy(this.gameObject);
+            Destroy(rocketCollider.gameObject);
+        }
+
+    }
+
+    private System.Collections.IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(destroyInSec);
+        Destroy(this.gameObject);
+    }
+}
