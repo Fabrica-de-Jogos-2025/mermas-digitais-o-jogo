338fe8c176147729d3b338612720a554ab0e06b9
JRMadara <joser.almir.324@gmail.com>
2025-03-30 22:51:09 -0300

    Feat:  cannon implementation.

---
+++ Mermãs Digitais O Jogo/Assets/Scripts/UI Scripts/Habilities/Teste_Die_5_3.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Teste_Die_5_3 : MonoBehaviour
+{
+
+    public Teste_Hability5_3 Dest;
+    //public float destroyInSec = 3f;
+    private bool isDestroyed = false;
+    //public GameObject rocket; // Referência ao foguete
+    //[SerializeField] private GameObject rocketCollider; // Collider do foguete
+    public GameObject habilityCard, Enemy5, Enemy6, Enemy7, Enemy8;
+
+    
+    void Update()
+    {
+        if (!isDestroyed && Dest.puzzleSolved)
+        {
+            //StartCoroutine(DestroyAfterDelay());
+            isDestroyed = true; // Evita chamadas repetidas da coroutine
+            habilityCard.SetActive(true);
+            //isDestroyed = true;
+            Destroy(Enemy5.gameObject);
+            Destroy(Enemy6.gameObject);
+            Destroy(Enemy7.gameObject);
+            Destroy(Enemy8.gameObject);
+
+            Destroy(this.gameObject);
+            //Destroy(rocket.gameObject);
+            //Destroy(rocketCollider.gameObject);
+        }
+    }
+
+    /*private System.Collections.IEnumerator DestroyAfterDelay()
+    {
+        yield return new WaitForSeconds(destroyInSec);
+        Destroy(this.gameObject);
+        //Destroy(rocket.gameObject);
+    }*/
+}
---
+++ Mermãs Digitais O Jogo/Assets/Scripts/UI Scripts/Habilities/Teste_Hability5_3.cs
@@ -0,0 +1,48 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class Teste_Hability5_3 : MonoBehaviour
+{
+    //[SerializeField] private TMP_InputField numberInput;
+    public Teste2_Hability5 Close;
+    public bool puzzleSolved = false;
+    
+    void Start()
+    {
+        //numberInput.characterValidation = TMP_InputField.CharacterValidation.Integer;
+    }
+
+    void update()
+    {
+        Verification();
+    }
+
+    public void Verification() 
+    {
+        if (Close.close)
+        {
+            puzzleSolved = true;
+        }
+        else
+        {
+            puzzleSolved = false;
+        }
+    }
+
+    /*public void NumberCaption() {
+        if (int.TryParse(numberInput.text, out int number))
+        {
+            if ((number == 0) && Teste2_Hability5.allDraggableObjects.TrueForAll(obj => obj.IsCorrectlyPlaced()))
+            {
+                puzzleSolved = true;
+            }
+            else if (number != 0)
+            {
+                puzzleSolved = false;
+            }
+        }
+    }*/
+}
+
